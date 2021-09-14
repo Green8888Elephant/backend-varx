6f8cc86b8f4d266c3b0a3cb69d865d6e727201c3
Green8888Elephant <53175180+Green8888Elephant@users.noreply.github.com>
2021-09-14 19:25:12 +0300

    Add new service to article

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd667e5ba293a4bf5c99d4123cf155a76'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a73282df-cca3-4259-98de-3d527ea6b48d'
+};
