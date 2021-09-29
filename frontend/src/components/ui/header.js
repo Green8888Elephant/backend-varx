868444f8cf790272840cda0c798cec671a3125f2
Green8888Elephant <53175180+Green8888Elephant@users.noreply.github.com>
2021-09-29 10:43:29 +0300

    Created Header Navigation

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,42 +1,109 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Hidden from "@material-ui/core/Hidden";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+import search from "../../images/search.svg";
+import cart from "../../images/cart.svg"
+import accountHeader from "../../images/account-header.svg"
+import menu from "../../images/menu.svg"
+
+const useStyles = makeStyles(theme => ({
+  coloredIndicator: {
+    backgroundColor: "#fff",
+  },
+  logoText: {
+    color: theme.palette.common.offBlack,
+  },
+  tabs: {
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  icon: {
+    height: '3rem',
+    width: '3rem',
+  },
+}))
+
+export default function Header({ categories}) {
+  const classes = useStyles();
+
+  const matchesMD = useMediaQuery((theme => theme.breakpoints.down("md")))
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const iOS =
+    typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const routes = [...categories, {node: {name: "Contact Us", strapiId: "contact"}}]
+
+  const tabs = (
+    <Tabs value={0} classes={{
+      indicator: classes.coloredIndicator,
+      root: classes.tabs
+    }}>
+      {routes.map(route =>(
+        <Tab label={route.node.name} key={route.node.strapiId}/>
+      ))}
+    </Tabs>
+  )
+
+  const drawer = (
+    <SwipeableDrawer
+      open={drawerOpen}
+      onOpen={() => setDrawerOpen(true)}
+      onClose={() => setDrawerOpen(false)}
+      disableBackdropTransition={!iOS} disableDiscovery={iOS}>
+      <List disablePadding>
+        {routes.map(route => (
+          <ListItem divider button key={route.node.strapiId}>
+            <ListItemText primary={route.node.name} />
+          </ListItem>
+        ))}
+      </List>
+    </SwipeableDrawer>
+  )
+
+  return (
+    <AppBar color="transparent" elevation={0}>
+      <Toolbar>
+        <Button>
+          <Typography variant="h1"><span className={classes.logoText}>VAR</span> X</Typography>
+        </Button>
+        {matchesMD ? drawer : tabs}
+        <IconButton>
+          <img
+            src={search}
+            alt="search"/>
+        </IconButton
+          >
+        <IconButton>
+          <img
+            className={classes.icon}
+            src={cart}
+            alt="cart"/>
+        </IconButton>
+        <IconButton
+        onClick={() => matchesMD ? setDrawerOpen(true) : null}>
+          <img
+            className={classes.icon}
+            src={matchesMD ? menu : accountHeader}
+            alt={matchesMD ? "menu" : "accountHeader"}
+          />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
